8c21e43efb7d2af9514dc8644906788ac96b5dbf
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-01-04 20:26:36 +0200

    commit

--- 242.valid-anagram.js
+++ 242.valid-anagram.js
@@ -20,6 +20,9 @@
 // Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 
 // Option 1
+// Time complexity of O(n), where n is the length of the strings:
+// Space complexity is O(1) because the character set is fixed to 26 lowercase English letters:
+
 const validAnagram = (s, t) => {
   // Step 1: Check if lengths are equal
   if (s.length !== t.length) {
@@ -30,15 +33,19 @@ const validAnagram = (s, t) => {
   const countS = {};
   const countT = {};
 
+  // Time complexity O(n) to loop through each character in s to build countS.
+  // Remember the string s could be very long so the number of iterations is proportional to the length of the string, n .
   for (let char of s) {
     countS[char] = (countS[char] || 0) + 1;
   }
 
+  // Time complexity O(n) to loop through each character in t to build countS
   for (let char of t) {
     countT[char] = (countT[char] || 0) + 1;
   }
 
   // Step 3: Compare the frequency maps
+  // Comparing the frequency maps is O(1) because the number of unique keys is at most 26 (constant for lowercase English letters).
   for (let key in countS) {
     if (countS[key] !== countT[key]) {
       return false;
@@ -49,7 +56,9 @@ const validAnagram = (s, t) => {
 };
 
 // Option 2
-
+// Sorting s & t takes O(n log n).
+// Comparing the two sorted strings takes O(n).
+// Sorting is computationally more expensive than creating frequency maps, especially for large strings.
 const validAnagram2 = (s, t) => {
   if (s.length !== t.length) return false;
   return s.split("").sort().join("") === t.split("").sort().join("");
