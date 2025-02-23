10927305c24286a68e00ea2bc9674b8193bee6a8
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-02-23 19:21:56 +0200

    commit

--- methods/array-methods.js
+++ methods/array-methods.js
@@ -1,5 +1,9 @@
 // Array Methods
 
+// Arrays are just a kind of object specialised for storing sequences of things.
+// If you evaluate typeof [], it produces "object".
+// You can visualise arrays as long, flat octopuses with all their tentacles in a neat row, labeled with numbers.
+
 // Here are some of the most important methods for arrays in JavaScript that are useful to remember.
 // These methods are fundamental for manipulating arrays in JavaScript and are commonly used in various programming tasks.
 
@@ -36,3 +40,22 @@ let hasTwo = arr.includes(2); // hasTwo is true
 
 // 11. slice(): Returns a shallow copy of a portion of an array or string into a new array or string.
 let slicedArr = arr.slice(0, 2); // slicedArr is [1, 2]
+
+// 12. indexOf method searches through the array and returns the index at which the requested value was found—or -1 if it wasn’t found.
+console.log([1, 2, 3, 2, 1].indexOf(2)); // → 1
+
+// 13. To search from the end instead of the start, there’s a similar method called lastIndexOf.
+console.log([1, 2, 3, 2, 1].lastIndexOf(2)); // → 3
+
+// 14. The concat method can be used to append arrays together to create a new array, similar to what the + operator does for strings.
+array = ["a", "b", "c", "d", "e"];
+array.slice(0, index).concat(array.slice(index + 1)); // → ["a", "b", "d", "e"]
+
+// 15. You can use three-dot notation (rest parameter) to call a function with an array of arguments.
+// This “spreads” out the array into the function call, passing its elements as separate arguments.
+let numbers = [5, 1, 7];
+console.log(Math.max(9, ...numbers, 2)); // → 7
+
+// 16. Square bracket array notation similarly allows the triple-dot operator to spread another array into the new array.
+let words = ["never", "fully"];
+console.log(["will", ...words, "understand"]); // → ["will", "never", "fully", "understand"]
--- methods/object-methods.js
+++ methods/object-methods.js
@@ -35,7 +35,18 @@ let hasA = obj.hasOwnProperty("a"); // hasA is true
 delete obj.b; // obj is now { a: 1 }
 
 // 10. JSON.stringify(): Converts a JavaScript object or value to a JSON string.
+// JSON serialises the data. That means it is converted into a flat description.
+// In JSON all property names have to be surrounded by double quotes, and only simple data expressions are allowed—no function calls, bindings, or anything that involves actual computation. Comments are not allowed in JSON.
 let jsonString = JSON.stringify(obj); // jsonString is '{"a":1}'
 
 // 11. JSON.parse(): Parses a JSON string, constructing the JavaScript value or object described by the string.
 let parsedObj = JSON.parse(jsonString); // parsedObj is { a: 1 }
+
+// 12. The binary in operator, when applied to a string and an object, tells you whether that object has a property with that name.
+let anObject = { left: 1, right: 2 };
+console.log("right" in anObject); // → true
+
+// 13. You can use three-dot notation (rest parameter) to “spread” out an object into a new object where it adds all properties from another object.
+// If a property is added multiple times, the last value to be added wins.
+let coordinates = { x: 10, y: 0 };
+console.log({ ...coordinates, y: 5, z: 1 }); // → {x: 10, y: 5, z: 1}
