ffde3b8fc5be516d34b765c617538bbaafac4bed
Karl-Alexander <99612323+KarlMeierMattern@users.noreply.github.com>
2025-04-28 20:25:13 +0200

    commit

--- 01.arrays_hashing/test.js
+++ 01.arrays_hashing/test.js
@@ -1,22 +1,10 @@
-const nums = [0, 3, 2, 5, 4, 6, 1, 1];
+const string = "cat";
 
-const longestConsecutiveSubString = (nums) => {
-  const numSet = new Set(nums);
-  let longestCount = 1;
-  for (let i = 0; i < nums.length; i++) {
-    if (!numSet.has(nums[i] - 1)) {
-      let currentNum = nums[i];
-      let currentCount = 1;
-
-      while (numSet.has(currentNum + 1)) {
-        currentNum++;
-        currentCount++;
-      }
-
-      longestCount = Math.max(longestCount, currentCount);
-    }
+const testIndex = (string) => {
+  if (string.includes("a")) {
+    return true;
   }
-  return longestCount;
+  return false;
 };
 
-console.log(longestConsecutiveSubString(nums));
+console.log(testIndex(string));
